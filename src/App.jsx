bc1b64bdcf59d4eb647336bc6d8ff60f70545318
Miguel Salazar <yobebe@hotmail.com.ar>
2023-11-28 00:14:17 -0300

    creando codigo para el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-//import React from 'react'
+import { useState, useEffect } from 'react';
+import axion from 'axion';
+import YouTube from 'react-youtube';
 
 const App = () => {
+  //Conecciones a las apis.
+  const apiURL = ''; //Desde aca voy a conectarme con la url de la api.
+  const apiKey = ''; //Desde aca me voy a conectar con la clave de la api. 
+  const imagePath = ''; //Desde aca voy a configurar las imagenes que va a mostrar. 
+  const imageYRL = ''; //Desde aca voy a conectarme con la url de las imagenes.
+
+  //Variables de estado.
+  const { movie, getMovie } = useState([]); 
+
   return (
     <div>
-      <h1> Hola a todos!! </h1>
-      <h2> Yo soy el leon!! </h2>
+      <h1> Peliculas con React </h1>
+      <p> En este sitio vas a poder buscar cualquier Peliculas </p>
     </div>
   )
 }
